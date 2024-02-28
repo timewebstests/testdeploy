ef239b964bd65ed9665210be077ab1727248c554
ervuji <mangovendo@gmail.com>
2024-02-28 22:57:13 +0100

    textchange

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world
-    </main>
-  );
+  return <main className="text-red-800">Hello world</main>;
 }
